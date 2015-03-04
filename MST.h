5831a11b52c0ff498d82ff14fb20a02eb69b97ce
Patchett <rybridges16@gmail.com>
2015-03-03 21:32:28 -0800

    Initial Commit

---
+++ MST.h
@@ -0,0 +1,36 @@
+#include "common.h"
+
+#pragma once
+
+class MST
+{
+public:
+    int **adjacentMatrix;
+    int **MSTMatrix;
+    int *parent; //Array to store constructed MST
+    int *key; //Key values used to pick minimum weight edge in cut
+    bool *mstSet; //To represent set of vertices not yet included in MST
+    int N; //the size of pointset
+
+    MST(int **adjacentMatrix, int size);
+    ~MST();
+
+    //deliverable a
+    void makeTree();
+    void printMST();
+
+    //deliverable b
+    void makeTSP2();
+
+    //deliverable c
+    void makeTSP1_5();
+
+    float calMean(int option);
+    float calStd(int option);
+
+private:
+    void minimumMatching();
+    void combine();
+    int minKey(int key[], bool mstSet[]);
+
+};
